a1f990bc0a6eda281689ebb1c9ae42df94e9f7bb
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-10-30 15:16:34 +0600

    day-2 : refactor Home.vue layout for improved structure and styling consistency

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,24 @@
 <template>
-  <main class="home-container">
-    <!-- Hero section (trailer/card area) -->
-    <section class="hero-section"></section>
+  <main class="home-container d-flex flex-column min-vh-100">
+    <section class="screen-section mb-4"></section>
 
-    <section class="home-section">
-      <h2>Trending Now</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Trending Now</h2>
     </section>
-    <section class="home-section">
-      <h2>Popular</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Popular</h2>
     </section>
-    <section class="home-section">
-      <h2>Top Rated</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Top Rated</h2>
     </section>
-    <section class="home-section">
-      <h2>Favourites</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Favourites</h2>
     </section>
-    <section class="home-section">
-      <h2>Upcoming Movies</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Upcoming Movies</h2>
     </section>
-    <section class="home-section">
-      <h2>Upcoming TV Series</h2>
+    <section class="px-5">
+      <h2 class="fs-3 fw-bold mb-4">Upcoming TV Series</h2>
     </section>
   </main>
 </template>
@@ -28,33 +27,20 @@
 
 <style scoped>
 .home-container {
-  display: flex;
-  flex-direction: column;
   gap: 2.5rem;
   background: var(--green-13);
-  min-height: 100vh;
 }
 
-.hero-section {
-  width: 100%;
+.screen-section {
   height: 43rem;
   background: linear-gradient(
     to right top,
     rgba(34, 118, 83, 0.75),
     rgba(13, 38, 28, 0.75)
   );
-  margin-bottom: 2rem;
-  /* Add shadow or gradient if you want */
-}
-
-.home-section {
-  padding: 0 2rem;
 }
 
 h2 {
   color: var(--green-1);
-  font-size: 1.5rem;
-  font-weight: 700;
-  margin-bottom: 1rem;
 }
 </style>
