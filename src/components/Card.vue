0f60bebb1ef259fb274e36056d11a5ac6f404dd5
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-03 15:09:10 +0600

    day-3 : Add Card component: Implement card display for items with dynamic image and title rendering

---
+++ src/components/Card.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import type { Show, Cast } from "../types/auth";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  item: {
+    type: Object as () => Show,
+    default: () => ({} as Show),
+  },
+  cast: {
+    type: Object as () => Cast,
+    default: () => ({} as Cast),
+  },
+  isCast: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <section class="mb-4">
+    <div
+      class="card d-flex flex-column align-items-center justify-content-end fw-bold rounded-3 shadow p-3 m-3"
+    >
+      <div v-if="isCast">
+        <div class="card-img w-100 rounded-2 mb-2">
+          <img
+            class="w-100 h-100 object-fit-cover rounded-2"
+            :src="cast.image || ''"
+            alt="Thumbnail"
+          />
+        </div>
+        <div class="card-title fw-bold text-center">
+          {{ cast.name }}
+        </div>
+      </div>
+      <div v-else>
+        <div class="card-img w-100 rounded-2 mb-2">
+          <img
+            class="w-100 h-100 object-fit-cover rounded-2"
+            :src="item.thumbnail || '../thumbnail.png'"
+            alt="Thumbnail"
+          />
+        </div>
+        <div class="card-title fw-bold text-center">
+          {{ item.title }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.card {
+  min-width: 180px;
+  height: 260px;
+  background: linear-gradient(
+    to right top,
+    rgba(18, 57, 49, 0.75),
+    rgba(13, 38, 28, 0.75)
+  );
+  transition: transform 0.3s cubic-bezier(0.4, 0.2, 0.2, 1), box-shadow 0.25s;
+}
+
+.card:hover {
+  transform: scale(1.12);
+  background-color: var(--green-1);
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.18);
+  font-weight: bolder;
+}
+
+.card-img {
+  height: 70%;
+  background: var(--green-7);
+}
+
+.card-title {
+  color: var(--white);
+}
+</style>
