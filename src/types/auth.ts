1140c97781aa9fdbe09a5952bd493df3f66d7a03
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-03 12:13:23 +0600

    day-3 : Add detailed Show type properties and implement item details in Detail component

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,5 +1,10 @@
 export type Show = {
+  id: string;
   title: string;
   thumbnail: string;
   description: string;
+  cast: string[];
+  release_year: number;
+  rating: number;
+  genres: string[];
 };
